48128af0109c975653a852c1d9ac0779682f1ab1
Srujan Gurram <srujangs8@gmail.com>
2023-11-21 11:47:54 +0530

    "Generate predictions with chosen LLMs in parallel and update the DataFrame with the predictions."

--- src/modules/generate_predictions.py
+++ src/modules/generate_predictions.py
@@ -3,6 +3,7 @@ from predictors.gpt_4_turbo import gpt_4_turbo
 from pandas import DataFrame
 from tqdm import tqdm
 from lib.prompts import issues_prediction_prompt
+from multiprocessing import Pool
 
 llm_choices_map = [
     {
@@ -16,32 +17,42 @@ llm_choices_map = [
 ]
 
 
+def make_prediction(args) -> tuple[str, str, int]:
+    """
+    Run the prediction with the given LLM
+    """
+    llm, contract_code, loc = args
+    prompt = issues_prediction_prompt.format(code=contract_code)
+    prediction = llm["func"](prompt)
+    return llm["name"], prediction, loc
+
+
 def generate_predictions(df: DataFrame, llm_choices: list[str]):
-    """
-    Run the prediction with the given LLMs
-    """
     # filter llm_choices_map to only include the chosen LLMs
-    chosen_llms = list(filter(lambda llm: llm["name"] in llm_choices, llm_choices_map))
+    chosen_llms = []
+    for llm in llm_choices_map:
+        if llm["name"] in llm_choices:
+            chosen_llms.append(llm)
 
-    # for every contract in the dataframe, run the prediction with chosen LLMs in parallel
-    # then, add the predictions to the dataframe
-    # return the dataframe
-
-    predictions_df = df.copy()
-    p_bar = tqdm(df.iterrows(), total=df.shape[0], desc="🧬 Generating predictions")
-
-    for contract in p_bar:
-        loc, code = contract[0], contract[1]["code"]
-
-        # run the predictions
-        predictions = []
+    # Prepare the arguments for multiprocessing
+    tasks = []
+    for loc, row in df.iterrows():
         for llm in chosen_llms:
-            prompt = issues_prediction_prompt.format(code=code)
-            prediction = llm["func"](prompt)
-            predictions.append(prediction)
+            tasks.append((llm, row["code"], loc))
 
-        # add the predictions to the dataframe
-        for i in predictions:
-            predictions_df.at[loc, "prediction"] = i
+    # Use multiprocessing Pool to run tasks in parallel
+    predictions = []
+    with Pool(8) as pool:
+        p_bar = tqdm(
+            pool.imap_unordered(make_prediction, tasks),
+            total=len(tasks),
+            desc="🧬 Generating predictions",
+        )
+        for result in p_bar:
+            predictions.append(result)
 
-    return predictions_df
+    # Update the DataFrame with the predictions
+    for llm_name, prediction, loc in predictions:
+        df.loc[loc, f"{llm_name}_prediction"] = prediction
+
+    return df
