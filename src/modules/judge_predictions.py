f3ec76f005701034f280a39ed4d53fe8bffdf1a9
Srujan Gurram <srujangs8@gmail.com>
2023-11-22 17:53:14 +0530

    added multiprocessing for judge predictions

--- src/modules/judge_predictions.py
+++ src/modules/judge_predictions.py
@@ -4,6 +4,7 @@ from lib.prompts import judge_predictions_prompt
 from tqdm import tqdm
 from json import loads
 from termcolor import cprint
+from multiprocessing import Pool
 
 
 def judge_predictions(df: DataFrame, llm_choices: list[str]) -> DataFrame:
@@ -12,37 +13,49 @@ def judge_predictions(df: DataFrame, llm_choices: list[str]) -> DataFrame:
     and appends {name-of-llm}_correct columns to the DataFrame
     which contains list of issue indices that were correctly predicted
     """
+
+    # Initialize columns in the DataFrame for each judgement type and llm_choice
     for llm_name in llm_choices:
-        all_false_negatives = []
-        all_false_positives = []
-        all_true_positives = []
+        for judgement_type in ["false_negatives", "false_positives", "true_positives"]:
+            column_name = f"{llm_name}_{judgement_type}"
+            if column_name not in df.columns:
+                df[column_name] = [None] * len(df)
 
-        # loop through each row in the DataFrame and compare the predictions
-        # to the actual issues
-        p_bar = tqdm(df.iterrows(), total=len(df), desc="🔎 Judging predictions")
-        for _, row in p_bar:
-            predicted_issues = row[f"{llm_name}_prediction"]
+    # prepare the arguments for multiprocessing
+    tasks = []
+    for llm in llm_choices:
+        for loc, row in df.iterrows():
+            predicted_issues = row[f"{llm}_prediction"]
             actual_issues = row["issues"]
-            judgement = produce_judgement(actual_issues, predicted_issues)
+            tasks.append((loc, llm, actual_issues, predicted_issues))
 
-            all_false_negatives.append(judgement["false_negatives"])
-            all_false_positives.append(judgement["false_positives"])
-            all_true_positives.append(judgement["true_positives"])
+    # Use multiprocessing Pool to run tasks in parallel
+    judgments = []
+    with Pool(8) as pool:
+        p_bar = tqdm(
+            pool.imap_unordered(make_judgement, tasks),
+            total=len(tasks),
+            desc="🔎 Judging predictions",
+        )
+        for result in p_bar:
+            judgments.append(result)
 
-        # update the DataFrame with the judgements
-        df[f"{llm_name}_false_negatives"] = all_false_negatives
-        df[f"{llm_name}_false_positives"] = all_false_positives
-        df[f"{llm_name}_true_positives"] = all_true_positives
+    # Update the DataFrame with the judgments
+    for loc, llm_name, judgement in judgments:
+        # here at each df loc, we insert false_negatives, false_positives, true_positives to their respective columns
+        for key, value in judgement.items():
+            df.at[loc, f"{llm_name}_{key}"] = value
 
     return df
 
 
-def produce_judgement(actual_issues: str, predicted_issues: str) -> dict:
+def make_judgement(args) -> tuple[int, str, dict]:
     """
     Compare the predictions to the actual issues and return the indices of the
     issues that were correctly predicted. This uses gpt-4-turbo to compare the
     issues.
     """
+    loc, llm_name, actual_issues, predicted_issues = args
     result = gpt_4_turbo(
         judge_predictions_prompt.format(
             key=format_key(actual_issues), prediction=predicted_issues
@@ -57,19 +70,23 @@ def produce_judgement(actual_issues: str, predicted_issues: str) -> dict:
         result = loads(result)
 
         # return the indices of the issues that were correctly predicted
-        return {
+        # we are doing this to make sure the format is consistent or else we produce error.
+        # see if we can do this better with output parsers from langchain
+        parsed_result = {
             "false_negatives": result["false_negatives"],
             "false_positives": result["false_positives"],
             "true_positives": result["true_positives"],
         }
+
+        return loc, llm_name, parsed_result
     except:
-        # if the result is not in json format, return an empty dictionary
         cprint(f"🚨 Error parsing judgement: {result}", "white", "on_red")
-        return {
+        error_result = {
             "false_negatives": None,
             "false_positives": None,
             "true_positives": None,
         }
+        return loc, llm_name, error_result
 
 
 def format_key(issues: list[str]) -> str:
