c8d1e572e0f3a81d0cd2a711e7b9ee0187f93157
Srujan Gurram <srujangs8@gmail.com>
2023-11-22 17:07:41 +0530

    added code for detection of false positives, false netitives and true positives

--- src/modules/judge_predictions.py
+++ src/modules/judge_predictions.py
@@ -2,6 +2,8 @@ from pandas import DataFrame
 from predictors.gpt_4_turbo import gpt_4_turbo
 from lib.prompts import judge_predictions_prompt
 from tqdm import tqdm
+from json import loads
+from termcolor import cprint
 
 
 def judge_predictions(df: DataFrame, llm_choices: list[str]) -> DataFrame:
@@ -11,7 +13,9 @@ def judge_predictions(df: DataFrame, llm_choices: list[str]) -> DataFrame:
     which contains list of issue indices that were correctly predicted
     """
     for llm_name in llm_choices:
-        new_correct_col = []
+        all_false_negatives = []
+        all_false_positives = []
+        all_true_positives = []
 
         # loop through each row in the DataFrame and compare the predictions
         # to the actual issues
@@ -19,15 +23,21 @@ def judge_predictions(df: DataFrame, llm_choices: list[str]) -> DataFrame:
         for _, row in p_bar:
             predicted_issues = row[f"{llm_name}_prediction"]
             actual_issues = row["issues"]
-            correct = correct_issues(actual_issues, predicted_issues)
-            new_correct_col.append(correct)
+            judgement = produce_judgement(actual_issues, predicted_issues)
 
-        df[f"{llm_name}_correct"] = new_correct_col
+            all_false_negatives.append(judgement["false_negatives"])
+            all_false_positives.append(judgement["false_positives"])
+            all_true_positives.append(judgement["true_positives"])
+
+        # update the DataFrame with the judgements
+        df[f"{llm_name}_false_negatives"] = all_false_negatives
+        df[f"{llm_name}_false_positives"] = all_false_positives
+        df[f"{llm_name}_true_positives"] = all_true_positives
 
     return df
 
 
-def correct_issues(actual_issues: str, predicted_issues: str) -> list[int]:
+def produce_judgement(actual_issues: str, predicted_issues: str) -> dict:
     """
     Compare the predictions to the actual issues and return the indices of the
     issues that were correctly predicted. This uses gpt-4-turbo to compare the
@@ -39,15 +49,27 @@ def correct_issues(actual_issues: str, predicted_issues: str) -> list[int]:
         )
     )
 
-    correct = []
     try:
-        if result != "N/A":
-            correct = list(map(int, result.split(",")))
-    except ValueError:
-        print("🚨 Error parsing judgement:", result)
-        pass
+        # remove the prompt from the result (only keep the json which should start from first { and end with last })
+        result = result[result.find("{") : result.rfind("}") + 1]
 
-    return correct
+        # parse the json result
+        result = loads(result)
+
+        # return the indices of the issues that were correctly predicted
+        return {
+            "false_negatives": result["false_negatives"],
+            "false_positives": result["false_positives"],
+            "true_positives": result["true_positives"],
+        }
+    except:
+        # if the result is not in json format, return an empty dictionary
+        cprint(f"🚨 Error parsing judgement: {result}", "white", "on_red")
+        return {
+            "false_negatives": None,
+            "false_positives": None,
+            "true_positives": None,
+        }
 
 
 def format_key(issues: list[str]) -> str:
--- src/predictors/gpt_4_turbo.py
+++ src/predictors/gpt_4_turbo.py
@@ -1,9 +1,9 @@
 from langchain.chat_models import ChatOpenAI
 
 
-def gpt_4_turbo(prompt: str):
+def gpt_4_turbo(prompt: str, **kwargs):
     """
     Run the GPT-4 Turbo LLM with the given prompt.
     """
-    model = ChatOpenAI(model="gpt-4-1106-preview")
+    model = ChatOpenAI(model="gpt-4-1106-preview", **kwargs)
     return model.predict(prompt)
