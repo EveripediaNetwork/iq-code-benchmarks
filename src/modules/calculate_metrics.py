d93b5ae5dc98d6cd5ebf793d1fa9c35e6fa4889b
Srujan Gurram <srujangs8@gmail.com>
2023-11-28 15:20:15 +0530

    Refactor calculate_metrics to update DataFrame with precision, recall, f1_score, and accuracy for each llm_name

--- src/modules/calculate_metrics.py
+++ src/modules/calculate_metrics.py
@@ -9,31 +9,28 @@ def calculate_metrics(df: DataFrame, llm_choices: list[str]) -> DataFrame:
     - Recall [True Positive / (True Positive + False Negative)]
     - Precision [True Positive / (True Positive + False Positive)]
     - F1 Score [2 * (Precision * Recall) / (Precision + Recall)]
-    - Accuracy  [True Positive + True Negative / (True Positive + False Positive + False Negative + True Negative)]
+    - Accuracy  [True Positive / (True Positive + False Positive + False Negative)]
     """
 
-    metrics_df = DataFrame()
-    metrics_df.columns = ["model", "recall", "precision", "f1_score", "accuracy"]
-
     for llm_name in llm_choices:
-        true_positives = df[f"{llm_name}_true_positives"].sum()
-        false_positives = df[f"{llm_name}_false_positives"].sum()
-        false_negatives = df[f"{llm_name}_false_negatives"].sum()
+        for i, row in df.iterrows():
+            true_positive = len(row[f"{llm_name}_true_positives"])
+            false_positive = len(row[f"{llm_name}_false_positives"])
+            false_negative = len(row[f"{llm_name}_false_negatives"])
 
-        recall = true_positives / (true_positives + false_negatives)
-        precision = true_positives / (true_positives + false_positives)
-        f1_score = 2 * (precision * recall) / (precision + recall)
+            recall = true_positive / (true_positive + false_negative)
+            precision = true_positive / (true_positive + false_positive)
 
-        metrics_df = metrics_df.append(
-            {
-                "model": llm_name,
-                "recall": recall,
-                "precision": precision,
-                "f1_score": f1_score,
-                "accuracy": (true_positives + true_positives)
-                / (true_positives + false_positives + false_negatives + true_positives),
-            },
-            ignore_index=True,
-        )
+            if precision + recall == 0:
+                f1_score = 0
+            else:
+                f1_score = 2 * (precision * recall) / (precision + recall)
 
-    return metrics_df
+            accuracy = true_positive / (true_positive + false_positive + false_negative)
+
+            df.loc[i, f"{llm_name}_recall"] = recall
+            df.loc[i, f"{llm_name}_precision"] = precision
+            df.loc[i, f"{llm_name}_f1_score"] = f1_score
+            df.loc[i, f"{llm_name}_accuracy"] = accuracy
+
+    return df
