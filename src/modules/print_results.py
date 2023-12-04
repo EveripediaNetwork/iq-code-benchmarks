170913426418c2eeceb632d9bef0d2b3a1dea5b6
Srujan Gurram <srujangs8@gmail.com>
2023-12-04 14:42:25 +0530

    updated cache and fixed IQ Score

--- src/modules/print_results.py
+++ src/modules/print_results.py
@@ -6,6 +6,14 @@ def print_results(df: DataFrame, llm_choices: list[str]):
     """
     Print the results of the benchmark to the console
     in a neat table
+
+    This aggregates the results of the benchmark
+    with the following metrics:
+    - Average Recall
+    - Average Precision
+    - Average F1 Score
+    - Average Accuracy
+    - IQ Score (weighted average of F1 Score on toy and real datasets)
     """
     results_df = DataFrame()
     for llm_name in llm_choices:
@@ -13,12 +21,16 @@ def print_results(df: DataFrame, llm_choices: list[str]):
         results_df.loc["Precision", llm_name] = df[f"{llm_name}_precision"].mean()
         results_df.loc["F1 Score", llm_name] = df[f"{llm_name}_f1_score"].mean()
         results_df.loc["Accuracy", llm_name] = df[f"{llm_name}_accuracy"].mean()
-        avg_toy_f1_score = (
-            results_df.loc["F1 Score"].filter(lambda x: x["type"] == "toy").mean()
-        )
-        avg_real_f1_score = (
-            results_df.loc["F1 Score"].filter(lambda x: x["type"] == "real").mean()
-        )
+
+        avg_toy_f1_score = 0
+        avg_real_f1_score = 0
+
+        for _, row in df.iterrows():
+            if row["type"] == "toy":
+                avg_toy_f1_score += row[f"{llm_name}_f1_score"]
+            else:
+                avg_real_f1_score += row[f"{llm_name}_f1_score"]
+
         results_df.loc["IQ Score", llm_name] = (
             0.2 * avg_toy_f1_score + 0.8 * avg_real_f1_score
         )
