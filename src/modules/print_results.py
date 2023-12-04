219171b6302ba3b255c638f7b906ba71f88e6089
Srujan Gurram <srujangs8@gmail.com>
2023-12-04 14:27:44 +0530

    Calculate and print the IQ score based on the average toy and real F1 scores

--- src/modules/print_results.py
+++ src/modules/print_results.py
@@ -13,5 +13,14 @@ def print_results(df: DataFrame, llm_choices: list[str]):
         results_df.loc["Precision", llm_name] = df[f"{llm_name}_precision"].mean()
         results_df.loc["F1 Score", llm_name] = df[f"{llm_name}_f1_score"].mean()
         results_df.loc["Accuracy", llm_name] = df[f"{llm_name}_accuracy"].mean()
+        avg_toy_f1_score = (
+            results_df.loc["F1 Score"].filter(lambda x: x["type"] == "toy").mean()
+        )
+        avg_real_f1_score = (
+            results_df.loc["F1 Score"].filter(lambda x: x["type"] == "real").mean()
+        )
+        results_df.loc["IQ Score", llm_name] = (
+            0.2 * avg_toy_f1_score + 0.8 * avg_real_f1_score
+        )
     cprint("🏋️ BENCH RESULTS", "green", attrs=["bold", "underline"])
     print(results_df)
